8fe3810738f9003821f5ab07102badf660814c94
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 18:23:57 +0100

    Fixed script structure.

--- scripts/04_04_Histogramme.py
+++ scripts/04_04_Histogramme.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-"""Erstellen einfacher Histogramme mit numpy und matplotlib.
+"""Erstellen einfacher Histogramme mit matplotlib.
 
-Histogramme lassen sich in Python mit der Funktion hist()
-erstellen. Als Parameter werden der Funktion der zu analysierende
-array und die bin-Grösse mitgegeben.
+Histogramme lassen sich in Python mit der Funktion hist() erstellen. Als
+Parameter werden der Funktion der zu analysierende array und die bin-Grösse
+mitgegeben.
 
 """
 
@@ -11,17 +11,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Erstellen von normalverteilten Daten
+# Erstellen eines normalverteilten Datensatzes
 x=np.random.normal(size=100)
+
+# Histogramm mit 5 bin-Klassen
+fig, axes = plt.subplots(1, 2)
+n, bins, _ = axes[0].hist(x, bins=5)
 
 # n: Anzahl der Werte im bin
 # bins: Grenzen der bins
 
-## Histogramm mit 5 bin-Klassen
-fig, axes = plt.subplots(1, 2)
-n, bins, _ = axes[0].hist(x, bins=5)
-
-## Histogramm mit vorgegebenen Grenzen
+# Histogramm mit vorgegebenen Grenzen
 n, bins, _ = axes[1].hist(x, range=[-3, 3])
 
-plt.show()
+plt.show() # Anzeigen des Plots
