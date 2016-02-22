d413794fb9db2c63a25ae7c9e5aa3fe1093fb98b
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 18:02:15 +0100

    Restructured script and inline documentation.

--- scripts/03_03_Speichern_von_Zahlenwerten_in_ASCII_Files.py
+++ scripts/03_03_Speichern_von_Zahlenwerten_in_ASCII_Files.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-"""Zahlenwerte in ASCII-files schreiben.
+"""Zahlenwerte in eine ASCII-Datei schreiben.
 
 Beim Datenaustausch ist es notwendig, seine Ergebnisse in einem für alle
-Anwender verwertbaren Format zu speichern. Hierbei bietet sich z.B.  das
+Anwender verwertbaren Format zu speichern. Hierbei bietet sich z.B. das
 Speichern der Daten in einer einfachen ASCII-Datei an.
 
 """
@@ -13,18 +13,17 @@ import numpy as np
 # Erstellen einer 5x5-Zufallsmatrix
 M = np.random.normal(size=(5, 5))
 
-# Speichern der Daten (M) mit Semikolon zur Trennung.
+# Speichern der Matrix M mit Semikolon als Trennungzeichen (Delimiter).
 np.savetxt('Testdatei_Semikolon.txt', M, delimiter=';')
 
 # Um einen anderen Delimiter zu verwenden, wird das ; einfach durch das
 # gewünschte Zeichen ersetzt. Neben dem Semikolon sind Komma (','), Tab ('\t')
 # und Leerzeichen (' ') gängige Delimiter.
 
-# Es ist sinnvoll seinem Datensatz eine Headerzeile hinzuzufügen, damit der
-# Inhalt leichter zu erkennen ist.
-header = 'A,B,C,D,E'
-np.savetxt('Testdatei_Header.txt', M, delimiter=',', header=header)
-
+# Es ist sinnvoll seinem Datensatz eine Kopfzeile (header) hinzuzufügen, in der
+# der Inhalt der Datei beschrieben wird.
 # Die Headerzeile ist per default ein Kommentar; ihr wird ein # vorangestellt.
 # Möchte man dieses Verhalten umgehen, kann man dies mit dem Keyword
 # comments=None ausschalten.
+header = 'A,B,C,D,E'
+np.savetxt('Testdatei_Header.txt', M, delimiter=',', header=header)
