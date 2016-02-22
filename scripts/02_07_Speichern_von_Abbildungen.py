fed548633d361e09d0865d68a78a6bac8232afb9
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 17:48:39 +0100

    Simplified script to only cover saving of files.

--- scripts/02_07_Speichern_von_Abbildungen.py
+++ scripts/02_07_Speichern_von_Abbildungen.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """Speichern von Abbildungen.
 
-Die Funktion savefig() bietet die Möglichkeit seine erstellen Grafiken direkt
-aus dem Skript heraus abzuspeichern.
+Die Funktion savefig() bietet die Möglichkeit erstellte Grafiken direkt aus dem
+Skript heraus abzuspeichern.
 
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Erstellen einer Testdatenreihe
@@ -15,18 +15,10 @@ x = np.linspace(0, 10, 100)
 y = x**2
 
 # Erstellen eines einfachen Plots
-fig1, ax = plt.subplots()
+fig, ax = plt.subplots()
 ax.plot(x, y)
 
-# Der Zugriff auf die figure erfolgt über die figure handle. Das Dateiformat
-# wird direkt im Dateinamen angegeben.
-fig1.savefig('./python_testplot.pdf')
-fig1.savefig('./python_testplot.png')
-
-# Wenn man die Abmessungen seiner Abbildungen bestimmen möchte, können diese
-# beim Aufruf der figure mitangegeben werden. Die Variable nimmt ein Tuple
-# entgegen, das (Breite, Höhe) in Inch beinhaltet.
-fig2, ax = plt.subplots(figsize=(15, 5))
-ax.plot(x, y)
-
-fig2.savefig('./python_testplot2.pdf')
+# Der Zugriff auf die figure erfolgt über die Variable fig. Das Dateiformat
+# wird direkt durch die Dateiendung bestimmt.
+fig.savefig('./python_testplot.pdf')
+fig.savefig('./python_testplot.png')
