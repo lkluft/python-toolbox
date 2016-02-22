fd944f5d7424c3b9c21474f39e6f54717208447d
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 17:40:14 +0100

    Alphabetical modul import.

--- scripts/02_02_Beispiel_Subplot.py
+++ scripts/02_02_Beispiel_Subplot.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
-"""Erstellen von subplots mit matplotlib.
+"""Erstellen von Subplots mit matplotlib.
 
 Um mehrere Plots in einer figure zu plotten, gibt es den Befehl subplot(). Seine
 Anwendung soll hier an einem einfachen Beispiel gezeigt werden.
 
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Erstellen eines x-Vektors (Nebenrechnung)
 x = np.linspace(0, 10, 100)
 
 # Erstellen einer figure mit zwei Plots nebeneinander.
+# plt.sublots(Reihen, Spalten)
 fig, axes = plt.subplots(1, 2)
 
-# Die Variable axes ist ein Array mit dessen Einträgen auf die einzelnen
+# Die Variable axes ist ein Array, mit dessen Einträgen auf die einzelnen
 # Subplots zugegriffen werden kann.
 axes[0].plot(x, np.sin(x))
 axes[0].set_title('Sinus')
