923242808bf88e50a0eff8bce5a8c3b79fdd25c5
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 17:58:55 +0100

    Improved inline documentation.

--- scripts/03_02_Einlesen_mehrerer_Dateien.py
+++ scripts/03_02_Einlesen_mehrerer_Dateien.py
@@ -12,23 +12,23 @@ import glob
 import numpy as np
 
 
-# Das Modul glob bietet die Möglichkeit mit den normalen Shell-Wildcards zu
-# arbeiten. So lassen sich zum Beispiel alle Dateien, deren Name  mit 'test'
-# beginnt und die die Endung '.txt' haben, in der Liste files speichern.
+# Das Modul glob bietet die Möglichkeit, mit den Shell-Wildcards zu arbeiten. So
+# lassen sich zum Beispiel alle Dateien, deren Name  mit 'test' beginnt und die
+# die Endung '.txt' haben, in der Liste files speichern.
 files = glob.glob('data/test*.txt')
 
-# Mit einer Schleife lässt sich im Anschluss auf die einzelnen Eintrage
-# der Liste zugreifen.
+# Mit einer Schleife lässt sich im Anschluss auf die einzelnen Eintrage der
+# Liste zugreifen.
 for filepath in files:
     with open(filepath) as f:
         data = np.genfromtxt(f)
     print('Inhalt von Datei ' + filepath)
     print(data)
 
-# Anstatt die einzelnen Arrays nur auszugeben, lassen sich diese auch in einer
-# Liste abspeichern. Hierzu muss die Liste vorher deklariert werden. Im
-# Anschluss kann der eingelesene Array mit der Funktion append an die Liste
-# angehängt werden.
+# Anstatt die einzelnen Arrays nur auszugeben, lassen sich diese beispielsweise
+# auch in einer Liste abspeichern. Hierzu muss die Liste vorher deklariert
+# werden. Im Anschluss kann der eingelesene Array mit der Funktion append an die
+# Liste angehängt werden.
 b = []
 for filepath in files:
     with open(filepath) as f:
