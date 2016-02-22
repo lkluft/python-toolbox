4edfca84edb190ed19245a31dabd9e27e64f4d00
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 18:06:24 +0100

    Changed format string. Removed file header.

--- scripts/03_04_Speichern_komplexer_Daten_in_ASCII_Files.py
+++ scripts/03_04_Speichern_komplexer_Daten_in_ASCII_Files.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-"""Formatierte Daten in ASCII-files schreiben.
+"""Daten in bestimmtem Format in eine ASCII-Datei schreiben.
+
+In einigen Fällen kann es erforderlich sein, ein spezielles Datenformat
+einzuhalten (Anzahl der Nachkommastellen, etc.). Hierzu kann beim Speichern das
+gewünscht Format explizit angegeben werden.
+
 """
 
 import numpy as np
@@ -7,25 +12,17 @@ import numpy as np
 
 # Erstellen von drei Vektoren mit unterschiedichen Daten zum späteren
 # Abspeichern. Dieser Schritt ist unnötig, wenn man beretis Daten hat.
-dat1 = np.linspace(1, 100, 100)
-dat2 = np.linspace(0, 10, 100)
+dat1 = np.arange(100)
+dat2 = np.linspace(0, 1, 100)
 dat3 = np.linspace(200, 1190, 100)
-
-## Speichern in ASCII-File
-# Erstellen einer Headerzeile
-header = "Ich bin eine Headerzeile"
 
 # Die Daten werden mit Hilfe der column_stack Funktion in einem array
 # zusammengefasst. Die ursprünglichen Vektoren werden zu Spalten im Vektor.
 stacked_data = np.column_stack((dat1, dat2, dat3))
 
 # Speichern der Daten mit eigenen Format fmt. Zur Nutzung der Formate siehe
-# Python-Dokumentation
+# Python-Dokumentation.
 np.savetxt('Testdatei_formatierte_Daten.txt',
            stacked_data,
-           fmt='%5.0f;%7.2f %7.2f',
-           header=header)
-
-# Die Headerzeile ist per default ein Kommentar; ihr wird ein # vorangestellt.
-# Möchte man dieses Verhalten umgehen, kann man dies mit dem Keyword
-# comments=None ausschalten.
+           fmt='%3d;%7.5f %7.2f'
+          )
