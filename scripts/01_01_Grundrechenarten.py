cd89307c29ddf82d08c6f8dd6c970e0072c06503
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 17:16:35 +0100

    Improved inline documentation.

--- scripts/01_01_Grundrechenarten.py
+++ scripts/01_01_Grundrechenarten.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Grundrechenarten mit Python und numpy.
 
-Im folgendenen Skript werden die Grundrechenarten in Python vorgestellt. Die
+Im folgenden Skript werden die Grundrechenarten in Python vorgestellt. Die
 Ergebnisse werden direkt an stdout (i.A. das Terminal) ausgegeben.
 
 """
@@ -11,19 +11,18 @@ import numpy as np
 
 # Grundrechenarten:
 print('Grundrechenarten')
-print(1+2)
-print(3-4)
-print(5*6)
-print(7/8)  # Ganzzahlige Division
-print(7./8) # Gleitkomma Division
-print(4**2)
+print(1 + 2) # Addition
+print(3 - 4) # Subtraktion
+print(5 * 6) # Multiplikation
+print(7 / 8)  # Ganzzahlige Division
+print(7. / 8) # Gleitkomma Division
+print(4**2) # Quadrat
 
 # Komplexere Rechenoperationen:
 print('Komplexere Rechenoperationen')
 
 # Funktionen aus einem Modul werden mittels Punktnotation aufgerufen:
-# modul.funktion
-
-print(np.sqrt(16))
-print(np.sin(np.pi/2))  # Auch Konstanten können in Modulen enthalten sein.
-print(np.cos(np.pi))
+# modul.funktion. Auch Konstanten können in Modulen enthalten sein.
+print(np.sqrt(16)) # Wurzel
+print(np.pi) # Pi
+print(np.sin(np.pi/2)) # Sinus
