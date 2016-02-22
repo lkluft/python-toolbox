8f4dd13bd5e21aebcec99314bf56559774882894
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 18:26:22 +0100

    Print the resulting indices.

--- scripts/04_05_Werte_finden.py
+++ scripts/04_05_Werte_finden.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 """Auffinden von Indizes bestimmter Werte.
+
+Bei der Arbeit mit Arrays ist es häufig nötig bestimmte Werte in einem Datensatz
+zu finden. Hierbei kann es nötig sein, die Indizes der betreffenden Werte in
+Erfahrung zu bringe.
+
 """
 
 import numpy as np
@@ -10,7 +15,7 @@ a = np.array([1, 2, 1, 3, 4, 1], dtype='float')
 print('Array: {0}'.format(a))
 
 # Gibt die Position (den Index) der Werte größer 2 an
-np.where(a>2)
+print(np.where(a>2))
 
 # Die ermittelten Indizes lassen sich wiederum als Input in den Array geben.
 # Somit lässt sich ein Array erzeugen, der alle Werte >2 aus dem Urpsrungsarray
@@ -19,6 +24,6 @@ b = a[np.where(a>2)]
 print('Werte >2: {0}'.format(b))
 
 # Ist man nicht an den Indizes interessiert, lässt sich dieser Array auch direkt
-# über die Logikabfrage erzeugen.
+# über eine logische Bedingung erzeugen.
 c = a[a>2]
 print('Werte >2: {0}'.format(c))
