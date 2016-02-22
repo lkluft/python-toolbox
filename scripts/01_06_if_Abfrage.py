4702edca5c16e5f7aeb54dc7a2d5b0145daf0083
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 17:27:10 +0100

    Restructured the script.

--- scripts/01_06_if_Abfrage.py
+++ scripts/01_06_if_Abfrage.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
-"""Steürung des Programmablaufes mittels if-Abfragen.
+"""Steuerung des Programmablaufes mittels if-Abfragen.
 
-Ein elementares Werkzeug der Prozesssteürung in der Programmierung sind
+Ein elementares Werkzeug der Prozesssteuerung in der Programmierung sind
 bedingte Abfagen. In Python lassen sich diese in Form von if-Konstrukten
-realisieren. Eine if-Abfrage steürt mit Hilfe einer Bedingung den Ablauf des
+realisieren. Eine if-Abfrage steuert mit Hilfe einer Bedingung den Ablauf des
 Skriptes.
 
 """
+
+# Anders als in anderen Programmiersprachen werden Anweisungen nicht
+# durch ein Keyword (z.B. endif, end, fi, ...) beendet oder eingeklammert.
+# Der Block wird leidglich durch Einrückung deklariert!
 
 x = 2
 
@@ -23,14 +27,10 @@ if ( x > 2 ) or ( x == 2 ):
 if ( x > 2 ) and (x == 2 ):
     print('Ich werde nicht ausgeführt.')
 
-# Mehrfache Abfrage
+# Geschachtelte Abfrage
 if x == 3:
     print('Ich werde zwar nicht ausgeführt.')
 elif x == 2:
     print('Ich aber.')
 else:
-    print('Ich würde ausgeführt werden, wenn nichts zutrifft.')
-
-# Anders als in anderen Programmiersprachen werden Anweisungen nicht
-# durch ein Keyword (z.B. endif, end, fi, ...) beendet oder eingeklammert.
-# Der Block wird leidglich durch Einrückung deklariert!
+    print('Ich würde ausgeführt, wenn keine Bedingung erfüllt ist.')
