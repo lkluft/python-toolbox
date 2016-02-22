1d6986de2ead84243ccf3df437b37d5583a19a79
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 17:43:26 +0100

    Fixed typo.

--- scripts/02_04_Plotten_mit_logaarithmischer_Achse.py
+++ scripts/02_04_Plotten_mit_logaarithmischer_Achse.py
@@ -3,19 +3,19 @@
 
 Bei der Visualisierung von Daten mit einem sehr großen Wertebereich kann eine
 halblogarithmische Darstellung hilfreich sein. Hierbei werden die Daten auf der
-y-Achse logarithmisch darfestellt.
+y-Achse logarithmisch dargestellt.
 
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Erstellen der Datenreihe
 x = np.linspace(0, 10, 100)
 y = np.exp(x)
 
-fig, axes = plt.subplots(1, 2) # s. 02_02_Beispiel_Subplots.py
+fig, axes = plt.subplots(1, 2)
 axes[0].semilogy(x, y) # logarithmische y-Achse
 axes[1].plot(x, y) # lineare y-Achse
 
-plt.show()
+plt.show() # Anzeigens des Plots
