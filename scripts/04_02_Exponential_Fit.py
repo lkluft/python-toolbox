89c3d2f56c14c6c05387ea3590dd4695bc5eedee
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 18:19:53 +0100

    Improved documentation. Unpack popt instead of explicit indexing.

--- scripts/04_02_Exponential_Fit.py
+++ scripts/04_02_Exponential_Fit.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Anpassen einer e-Funktion an einen Datensatz.
 
-Exponentielles Fitten ist in ein häufiger Schritt bei der Auswertung von
+Exponentielles Fitten ist ein häufiger Schritt bei der Auswertung von
 Messreihen. In Python bietet das Modul scipy.optimize eine curve_fit() Funktion.
 Diese ermöglicht das Fitten von Daten an eine beliebige Funktion.
 
@@ -24,13 +24,13 @@ def func(x, a, b, c):
 # Funktion, die x-Werte und die y-Werte mitgegeben.
 popt, pcov = curve_fit(func, x, y)
 
-# Die Variable popt ist ein Array, der die Werte für die Parameter a, b und c
-# beinhaltet. pcov enthält die geschätzte Kovarianz von popt.
+# popt: Array, das die Werte für die Parameter a, b und c beinhaltet.
+# pcov: Geschätzte Kovarianz von popt.
 
-# Plot der Datenreihe in Punktdarstellung sowie der gefitteten Funktion
+# Plot der Punktwolke und der gefitteten Funktion
 fig, ax = plt.subplots()
 ax.plot(x, y, marker='x', markersize=5, linestyle='none')
-ax.plot(x, func(x, popt[0], popt[1], popt[2]), color='r')
+ax.plot(x, func(x, *popt), color='r')
 ax.legend(['Data','Fit'],loc='upper left')
 
-plt.show()
+plt.show() # Anzeigen des Plots
