af44d1ba099ab89913fef9f0d255cb9609e90bcb
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 18:13:08 +0100

    Improved inline documentation.

--- scripts/04_01_Lineares_Fitten.py
+++ scripts/04_01_Lineares_Fitten.py
@@ -11,16 +11,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Erstellen einer Zufallsreihe
+# Erstellen einer Zufallsreihe mit zufälligem Rauschen
 x = np.linspace(1, 10, 100)
 y = 0.5 * x + np.random.uniform(-1, 1, size=100)
 
-# Ausgleichsgerade 1. Ordnung erstellen ( f(x)=a(1)*x+a(2) )
+# Ausgleichsgerade 1. Ordnung erstellen [ f(x) = a(1) * x + a(2) ]
 a = np.polyfit(x, y, 1)
 
-# Plotten der Angleichsgeraden durch die Punktwolke
+# Plotten der Ausgleichsgeraden durch die Punktwolke
 fig, ax = plt.subplots()
 ax.plot(x, y, marker='x', linestyle='none') # Plot der Zahlenreihe
 ax.plot(x, np.polyval(a, x), 'r') # Plot des Fits
 
-plt.show()
+plt.show() # Anzeigen des Plots
