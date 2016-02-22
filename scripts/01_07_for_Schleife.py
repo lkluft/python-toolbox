b9156848a453fe7318458cd868489ff086ecbe66
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-22 17:34:05 +0100

    Made structure and documentation more clear.

--- scripts/01_07_for_Schleife.py
+++ scripts/01_07_for_Schleife.py
@@ -4,21 +4,25 @@
 Schleifen stellen eine wichtige Funktion in der Datenverarbeitung dar.
 Sie ermöglichen das wiederholte Ausführen von Anweisungsblöcken.
 
+In diesem Skript wird eine for-Schleife verwendet um die ersten 20
+Fibonacci-Zahlen zu berechnen.
+
 """
 
 import numpy as np
 
 
-# Fibonacci-Folge mit for-Schleife
-
+# Initialisierung eines Arrays mit Nullen.
 F = np.zeros(20, dtype='int')
+
+# F[0]=0, F[1]=1
 F[1] = 1
 
-# Die Schleife läuft von 2 bis 19 (!). Nach Berechnung der jeweiligen
-# Fibonacci-Zahl wird diese ausgegeben.
+# Die Schleife läuft von 2 an über alle Einträge im Array. Nach Berechnung der
+# jeweiligen Fibonacci-Zahl wird diese ausgegeben.
 for i in range(2, len(F)):
     F[i] = F[i-1] + F[i-2]
     print('F({0}) = {1}'.format(i, F[i]))
 
 # Die Deklaration, über welchen Block die Schleife laufen soll, erfolgt wie bei
-# der if-Abfrage (s. if_Abfrage.py) per Einrückung der betroffenen Zeilen.
+# der if-Abfrage (s. 01_06_if_Abfrage.py) per Einrückung der jeweiligen Zeilen.
