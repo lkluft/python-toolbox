ded9b37497ea6ed8eea2b49137f90ea8a1b338f8
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-26 15:05:37 +0100

    Major overhaul.
    The dtypes are not passed explicitly anymore. This may be slower but simplifies
    the reading routine a lot. Also the column names defined in the file header are
    used.
    
    Use fig and ax objects while plotting for consistency.

--- scripts/02_05_Plot_mit_Zeitachse.py
+++ scripts/02_05_Plot_mit_Zeitachse.py
@@ -13,29 +13,28 @@ import numpy as np
 with open('data/Windkanal_Testdaten.csv', 'rb') as file:
     file = (line.decode().replace(',','.').encode() for line in file)
     data = np.genfromtxt(file,
-                  dtype=[('index', 'i1'),
-                         ('date', 'S10'),
-                         ('time', 'S8'),
-                         ('speed', 'f8')],
-                  delimiter=';',
-                  skip_header=1)
+                         dtype=None,
+                         names=True,
+                         delimiter=';',
+                        )
 
 # Im nächsten Schritt wird die eingelesene Zeit in einen Zahlenwert umgewandelt.
 # Somit lässt sich die Variable time als x-Wert für einen Plot nutzen.
-time = mpl.dates.datestr2num(data['time'])
+time = mpl.dates.datestr2num(data['TIME'])
 
 # Die Funktion plot_date ermöglicht es, den Zeitplot zu erstellen.
 # Eingangsvariablen sind die erzeugte time Variable und die y-Werte.  Zusätzlich
 # wird angegeben, welche Variablen ein Datumsformat sind.  Der Parameter
-# definiert den Plot (hier blaü x)
-plt.plot_date(time, data['speed'], xdate=True, ydate=False, fmt='bx')
-plt.ylabel('Windgeschwindigkeit')
-plt.xlabel('Uhrzeit')
+# definiert den Plot (hier blaue Punkte)
+fig, ax = plt.subplots()
+ax.plot_date(time, data['M40V02'], xdate=True, ydate=False, fmt='bo')
+ax.set_ylabel('Windgeschwindigkeit')
+ax.set_xlabel('Uhrzeit')
 
 # Mit dem DateFormatter lässt sich das auf der Achse angezeigte Datumsformat
 # editieren.
 formatter = mpl.dates.DateFormatter('%H:%M:%S')
-plt.gcf().axes[0].xaxis.set_major_formatter(formatter)
+ax.xaxis.set_major_formatter(formatter)
 #plt.xticks(rotation=45) # Auskommentieren um die x-Ticks zu drehen
 
 plt.show()
