ac2eec4510754e10540dc1e1cba9ad03b6d58857
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-27 12:43:30 +0100

    Added scripts to explain variables and datatypes.
    * scripts/01_02_Variablen.py:
    
    Definition and usage of variables. Currently containing a docstring.
    
    * scripts/01_03_Datentypen.py:
    
    Introduce some built-in datatypes. Currently containing a docstring.
    
    * misc:
    
    Renamed a couple of files so that the numbering matches the table of contents.

---
+++ scripts/01_02_Variablen.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Deklaration von Variablen.
+
+Die Deklaration und Nutzung von Variablen ist ein essentieller Bestandteil der
+Programmierung. In diesem Skript wird kurz erläutet, wie Variablen angelegt und
+genutzt werden.
+
+"""
---
+++ scripts/01_03_Datentypen.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Eingebaute Datentypen.
+
+Python bietet von sich aus einige grundlegende eingebaut Datentypen an
+(built-in datatypes). Dieses Skript stellt eine Auswahl dieser Datentypen vor,
+welche im Alltag der Datenverarbeitung häufig genutzt werden. Eine vollstädnige
+Liste findet sich in der Python Dokumentation [0].
+
+[0] https://docs.python.org/2/library/stdtypes.html
+
+"""
