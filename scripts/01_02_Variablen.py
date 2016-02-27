0556b989896d9ded16cc72bb54932b6f1233ef6f
Lukas Kluft <lukas.kluft@gmail.com>
2016-02-27 13:22:51 +0100

    Added content.

--- scripts/01_02_Variablen.py
+++ scripts/01_02_Variablen.py
@@ -6,3 +6,14 @@ Programmierung. In diesem Skript wird kurz erläutet, wie Variablen angelegt und
 genutzt werden.
 
 """
+
+# Die Zuweisung eines Wertes zu einer Variablen erfolgt über den Opterator '='.
+var = 2
+
+# Rechnen mit Variablen.
+var2 = var * 2 # var2 hält den Wert 4
+
+# Manipulation einer Variablen.
+# Möchte man den Wert einer Variablen verändern (x = x * 2) bietet Python die
+# folgende Syntax. var muss in diesem Fall nur einmal aufgerufen werden.
+var *= 2 # auch +=, -=, /=
