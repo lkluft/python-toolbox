82f79fad4c02349706033da22f52b062512d41fd
Lukas Kluft <lukas.kluft@gmail.com>
2018-03-21 08:44:18 +0100

    Replace ambiguous variable names (PEP8 fix).
    Comply with PEP8 E741: Do not use 'I', 'l' or 'O' as variable names.
    
    Make all other variable names more verbose, too.

--- scripts/datentypen.py
+++ scripts/datentypen.py
@@ -4,32 +4,33 @@
 Python bietet von sich aus einige grundlegende eingebaut Datentypen an
 (built-in datatypes).  Dieses Skript stellt eine Auswahl dieser
 Datentypen vor, welche im Alltag der Datenverarbeitung häufig genutzt
-werden.  Eine vollstädnige Liste findet sich in der Python Dokumentation
-[0].
+werden.  Eine vollstädnige Liste findet sich in der Python Dokumentation:
 
-[0] https://docs.python.org/2/library/stdtypes.html
+    https://docs.python.org/2/library/stdtypes.html
 
 """
 
 # Basistypen
 
-i = 1  # Integer (Ganzzahlen)
-f = 1.0  # Float (Gleitkommazahl)
-s = 'eins'  # String (Zeichenkette)
-b = True  # Boolean (Boolscher Wahrheitswert)
-
+print('integer:', 1)  # Ganzzahlen
+print('float:', 1.0)  # Gleitkommazahl
+print('string:', 'eins')  # Zeichenkette
+print('boolean:', True)  # Boolscher Wahrheitswert
 
 # Erweiterte Datentypen
 
-# Listen dienen zum Zusammenfassen mehrerer Variablen.  Die Einträge
-# einer Liste haben eine feste Reihenfolge.  Listen lassen sich
-# erweitern und sortieren.
-l = [2, 1, 3]
-l.append(4)  # l = [2, 1, 3, 4]
-l.sort()  # l = [1, 2, 3, 4]
+# Listen dienen zum Zusammenfassen mehrerer Variablen.
+# Die Einträge einer Liste haben eine feste Reihenfolge.
+int_list = [2, 1, 3]
 
-# Dictionaries bieten die Möglichkeit Daten unter einem bestimmten 'key'
+# Listen lassen sich erweitern...
+int_list.append(4)  # int_list = [2, 1, 3, 4]
+
+# und sortieren.
+int_list.sort()  # int_list = [1, 2, 3, 4]
+
+# Dictionaries bieten die Möglichkeit Daten unter einem bestimmten `key`
 # abzuspeichern.  Der Zugriff auf den gespeicherten Wert erfolgt dann
 # über den jeweiligen key.
-d = {'key': 1}
-var = d['key']  # var = 1
+dictionary = {'key': 1}
+var = dictionary['key']  # var = 1
